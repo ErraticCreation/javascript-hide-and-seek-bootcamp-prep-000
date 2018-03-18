cfef22dca2f047751057d8f6a0f99a54952e2cdc
Tarah Nixon <tarah.sage.nixon@gmail.com>
2018-03-18 19:06:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  document.querySelector("ul.ranked-list ");
+  let rankedListData = document.querySelector("ul.ranked-list li");
+  
+  for (let i = 0; i < rankedListData.length; i++) {
+    rankedListData[i].innerHTML = rankedListData[i].innerHTML + 1;
+  }
 }
 
 function deepestChild() {
