ebb05b459d46c977f1b33bf756ff0254b6e96474
Tarah Nixon <tarah.sage.nixon@gmail.com>
2018-03-18 19:10:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.querySelector("#nexted div.target");
+  return document.querySelector("#nexted div div div div.target");
 }
 
 function increaseRankBy(n) {
